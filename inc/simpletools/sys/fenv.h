3986a4e2b64bdb3ca6f249370c8d9f0d9f056714
DerekStride <djgstride@gmail.com>
2015-03-28 16:34:49 -0400

    Removed simpletools

--- inc/simpletools/sys/fenv.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SYS_FENV_H
-#define _SYS_FENV_H
-
-  typedef struct fenv_struct {
-    unsigned char exceptions;
-    unsigned char roundingmode;
-  } _fenv_t;
-
-#endif
--- inc/simpletools/sys/size_t.h
+++
@@ -1,8 +0,0 @@
-#ifndef _SIZE_T
-
-#ifndef __SIZE_TYPE__
-#define __SIZE_TYPE__ unsigned long
-#endif
-  typedef __SIZE_TYPE__ size_t;
-#define _SIZE_T
-#endif
