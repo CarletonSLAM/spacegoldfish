81b63fef89d2ea38a72497f037ad6e8e145a2f31
Krishna Mannem <krishna@dhcp-66-167.cu-internet.carleton.ca>
2015-04-17 18:01:32 -0400

    Working on I2C code

--- gcc/main.c
+++ gcc/main.c
@@ -1,87 +1,76 @@
-#include "CU_TM4C123.h"
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "inc/hw_i2c.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_gpio.h"
+#include "driverlib/i2c.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/uart.h"
+
+void initI2C1(void);
+uint32_t I2CReceive(uint32_t slaveAddress, uint8_t reg);
 
 int main(void){
+    uint32_t temp;
+    uint32_t i;
+    
+    ROM_SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+    initI2C1();
 
-    //volatile to prevent GCC from optimizing out variable.
-    volatile uint32_t ui32Loop;
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
 
-    // Enable GPIOF
-	SYSCTL->RCGCGPIO |= (1UL << 5);
-	
-    // Enable the GPIO pin as output
-	GPIOF->DIR |= 0x0F;     
-
-    // enable digital output
-    GPIOF->DEN |= 0x0F;       
-
-   // Enable GPIOB
-    SYSCTL->RCGCGPIO |= 0x02;
-
-    // Set up GPIOB as UART1
-    GPIOB->AFSEL |= 0x01;
-    GPIOB->PCTL |= 0x01;
-
-
-    /*
-     *	Set bits 0, 8, 9.
-     *	Bit 0 - UART Enable
-     *	Bit 8 - UART Transmit Enable
-     *	Bit 9 - UART Receive Enable
-     */
-    UART0->CTL |= 0x0301;
-
-    // TODO - Section 14.3.2
-    //UART0->IBRD
-    //UART0->FBRD
-
-	while(1)
-    {
-        // Turn on the LED.
-        GPIOF->DATA |= 0x04;
-
-        // busy wait
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++);
-
-        // Turn off the LED.
-        GPIOF->DATA &= ~(0x04);
-
-        // Busy wait.
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++);
-
-        // Turn on the LED.
-        GPIOF->DATA |= 0x08;
-
-        // busy wait
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++);
-
-        // Turn off the LED.
-        GPIOF->DATA &= ~(0x08);
-
-        // Busy wait.
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++);
-
-        // Turn on the LED.
-        GPIOF->DATA |= 0x02;
-
-        // busy wait
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++);
-
-        // Turn off the LED.
-        GPIOF->DATA &= ~(0x02);
-
-        // Busy wait.
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++);
-
-        // Turn on the LED.
-        GPIOF->DATA |= 0x01;
-
-        // busy wait
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++);
-
-        // Turn off the LED.
-        GPIOF->DATA &= ~(0x01);
-
-        // Busy wait.
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++);
+    ROM_GPIOPinConfigure(GPIO_PA0_U0RX);
+    ROM_GPIOPinConfigure(GPIO_PA1_U0TX);
+    ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+    ROM_UARTConfigSetExpClk(UART0_BASE, ROM_SysCtlClockGet(), 115200, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+    while (1) {
+        for (i = 0; i<1000;i++);
+        temp = I2CReceive(0x0E, 0x01);
+        UARTSend((uint8_t*) temp, 1);
+        ROM_UARTCharPutNonBlocking(UART0_BASE, (char)temp);
     }
+}
+
+void UARTSend(const uint8_t *pui8Buffer, uint32_t ui32Count)
+{
+    //
+    // Loop while there are more characters to send.
+    //
+    while(ui32Count--)
+    {
+        //
+        // Write the next character to the UART.
+        //
+        ROM_UARTCharPutNonBlocking(UART0_BASE, *pui8Buffer++);
+        while (ROM_UARTBusy(UART0_BASE));
+    }
+}
+
+void initI2C1(void) {
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C1);
+    ROM_SysCtlPeripheralReset(SYSCTL_PERIPH_I2C1);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    ROM_GPIOPinConfigure(GPIO_PA6_I2C1SCL);
+    ROM_GPIOPinConfigure(GPIO_PA7_I2C1SDA);
+    ROM_GPIOPinTypeI2CSCL(GPIO_PORTA_BASE, GPIO_PIN_6);
+    ROM_GPIOPinTypeI2C(GPIO_PORTA_BASE, GPIO_PIN_7);
+    ROM_I2CMasterInitExpClk(I2C1_BASE, ROM_SysCtlClockGet(), false);
+    HWREG(I2C1_BASE + I2C_O_FIFOCTL) = 80008000;
+}
+
+uint32_t I2CReceive(uint32_t slaveAddress, uint8_t reg) {
+    ROM_I2CMasterSlaveAddrSet(I2C1_BASE, slaveAddress, false);
+    ROM_I2CMasterDataPut(I2C1_BASE, reg);
+    ROM_I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_START);
+    while(ROM_I2CMasterBusy(I2C1_BASE));
+    ROM_I2CMasterSlaveAddrSet(I2C1_BASE, slaveAddress, true);
+    ROM_I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_SINGLE_RECEIVE);
+    while(ROM_I2CMasterBusy(I2C1_BASE));
+    return ROM_I2CMasterDataGet(I2C1_BASE);
 }
