7371eb53fe8c99c4d3e9df9011c148504c0b51e2
Hasith Vidanamadura <h@vidanamadura.net>
2015-04-17 20:14:50 -0500

    Fixed I2C implementation

--- gcc/main.c
+++ gcc/main.c
@@ -30,11 +30,10 @@ int main(void){
     ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
     ROM_UARTConfigSetExpClk(UART0_BASE, ROM_SysCtlClockGet(), 115200, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
     while (1) {
-        for (i = 0; i<1000;i++);
-        temp = I2CReceive(0x0E, 0x01);
+        for (i = 0; i<1000;i++){        temp = I2CReceive(0x0E, 0x01);
         UARTSend((uint8_t*) temp, 1);
         ROM_UARTCharPutNonBlocking(UART0_BASE, (char)temp);
-    }
+    }}
 }
 
 void UARTSend(const uint8_t *pui8Buffer, uint32_t ui32Count)
@@ -53,13 +52,13 @@ void UARTSend(const uint8_t *pui8Buffer, uint32_t ui32Count)
 }
 
 void initI2C1(void) {
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C1);
-    ROM_SysCtlPeripheralReset(SYSCTL_PERIPH_I2C1);
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+   ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
     ROM_GPIOPinConfigure(GPIO_PA6_I2C1SCL);
     ROM_GPIOPinConfigure(GPIO_PA7_I2C1SDA);
     ROM_GPIOPinTypeI2CSCL(GPIO_PORTA_BASE, GPIO_PIN_6);
     ROM_GPIOPinTypeI2C(GPIO_PORTA_BASE, GPIO_PIN_7);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C1);
+    ROM_SysCtlPeripheralReset(SYSCTL_PERIPH_I2C1);
     ROM_I2CMasterInitExpClk(I2C1_BASE, ROM_SysCtlClockGet(), false);
     HWREG(I2C1_BASE + I2C_O_FIFOCTL) = 80008000;
 }
@@ -70,7 +69,9 @@ uint32_t I2CReceive(uint32_t slaveAddress, uint8_t reg) {
     ROM_I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_START);
     while(ROM_I2CMasterBusy(I2C1_BASE));
     ROM_I2CMasterSlaveAddrSet(I2C1_BASE, slaveAddress, true);
-    ROM_I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_SINGLE_RECEIVE);
+    ROM_I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_RECEIVE_FINISH);
+
     while(ROM_I2CMasterBusy(I2C1_BASE));
+//  ROM_I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_RECEIVE_FINISH);
     return ROM_I2CMasterDataGet(I2C1_BASE);
 }
