d6eb4631d23a20e66aa8ba85c9f508e5f89297ae
Krishna Mannem <krishna@krishna.local>
2015-04-21 22:43:03 -0400

    changes to I2C for altimeter

--- gcc/main.c
+++ gcc/main.c
@@ -13,28 +13,34 @@
 #include "driverlib/uart.h"
 
 void initI2C1(void);
-uint32_t I2CReceive(uint32_t slaveAddress, uint8_t reg);
+void I2CReceive(uint32_t slaveAddress, char retData[]);
+void resetCMD(void);
+char promRead();
 
 int main(void){
-    uint32_t temp;
-    uint32_t i;
+
+    char temp[3];
     
     ROM_SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
     initI2C1();
 
+    ///////////UART SHIT
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-
     ROM_GPIOPinConfigure(GPIO_PA0_U0RX);
     ROM_GPIOPinConfigure(GPIO_PA1_U0TX);
     ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
     ROM_UARTConfigSetExpClk(UART0_BASE, ROM_SysCtlClockGet(), 115200, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
-    while (1) {
-        for (i = 0; i<1000;i++);
-        temp = I2CReceive(0x0E, 0x01);
-        UARTSend((uint8_t*) temp, 1);
-        ROM_UARTCharPutNonBlocking(UART0_BASE, (char)temp);
-    }
+    ///////////////////////
+
+    resetCMD();
+    I2CReceive(0xEE, temp);
+    temp[1] = promRead();
+    ROM_UARTCharPutNonBlocking(UART0_BASE, temp[0]);
+    ROM_UARTCharPutNonBlocking(UART0_BASE, temp[1]);
+    ROM_UARTCharPutNonBlocking(UART0_BASE, temp[2]);
+
+
 }
 
 void UARTSend(const uint8_t *pui8Buffer, uint32_t ui32Count)
@@ -62,15 +68,47 @@ void initI2C1(void) {
     ROM_GPIOPinTypeI2C(GPIO_PORTA_BASE, GPIO_PIN_7);
     ROM_I2CMasterInitExpClk(I2C1_BASE, ROM_SysCtlClockGet(), false);
     HWREG(I2C1_BASE + I2C_O_FIFOCTL) = 80008000;
+
 }
 
-uint32_t I2CReceive(uint32_t slaveAddress, uint8_t reg) {
-    ROM_I2CMasterSlaveAddrSet(I2C1_BASE, slaveAddress, false);
-    ROM_I2CMasterDataPut(I2C1_BASE, reg);
-    ROM_I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_START);
+void resetCMD(void) {
+    ROM_I2CMasterSlaveAddrSet(I2C1_BASE, 0xEE, false);
+    ROM_I2CMasterDataPut(I2C1_BASE, 0x1E);
+    ROM_I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_SINGLE_SEND);
     while(ROM_I2CMasterBusy(I2C1_BASE));
-    ROM_I2CMasterSlaveAddrSet(I2C1_BASE, slaveAddress, true);
+    return;
+}
+
+char promRead() {
+    ROM_I2CMasterSlaveAddrSet(I2C1_BASE, 0xEE, false);
+    ROM_I2CMasterDataPut(I2C1_BASE, 0xA6);
+    ROM_I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_SINGLE_SEND);
+    while(ROM_I2CMasterBusy(I2C1_BASE));
+    ROM_I2CMasterSlaveAddrSet(I2C1_BASE, 0xEE, true);
     ROM_I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_SINGLE_RECEIVE);
     while(ROM_I2CMasterBusy(I2C1_BASE));
     return ROM_I2CMasterDataGet(I2C1_BASE);
+}
+
+void I2CReceive(uint32_t slaveAddress, char retData[]) {
+    ROM_I2CMasterSlaveAddrSet(I2C1_BASE, 0xEE, false);
+    ROM_I2CMasterDataPut(I2C1_BASE, 0x42);
+    ROM_I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_START);
+    while(ROM_I2CMasterBusy(I2C1_BASE));
+    ROM_I2CMasterDataPut(I2C1_BASE, 0x00);
+    ROM_I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_FINISH);
+    while(ROM_I2CMasterBusy(I2C1_BASE));
+    ROM_I2CMasterSlaveAddrSet(I2C1_BASE, 0xEE, true);
+
+    ROM_I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_RECEIVE_START);
+    while(ROM_I2CMasterBusy(I2C1_BASE));
+    retData[0] = ROM_I2CMasterDataGet(I2C1_BASE);
+
+    ROM_I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_RECEIVE_CONT);
+    while(ROM_I2CMasterBusy(I2C1_BASE));
+    retData[1] = ROM_I2CMasterDataGet(I2C1_BASE);
+
+    ROM_I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_RECEIVE_FINISH);
+    while(ROM_I2CMasterBusy(I2C1_BASE));
+    retData[2] = ROM_I2CMasterDataGet(I2C1_BASE);
 }
